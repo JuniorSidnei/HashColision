cf4cc7b3e298127468e8c73e21d54da3137c5dcf
Sidnei <Sidnei>
2017-04-05 16:57:32 -0300

    Exercixio colisao hash

---
+++ Exercicio/Source.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define tamanho 1000
+#define keyMAx 9999
+
+
+FILE *saida; 
+
+int main()
+{
+	saida = fopen("C:\\Users\\Sidnei\\Source\\Repos\\NewRepo\\ExercicioColisao\\ExercicioColisao\\saida.txt", "wt");
+
+	int vet[tamanho], vet2[tamanho], vet3[tamanho],key = 1, contColision = 0, modKey = 0;
+
+	//Chaves colocadas no vetor de 1 até 999
+	for (int i = 0; i < tamanho; i++)
+	{
+		key = i;
+		vet[i] = key;
+		vet3[i] = 0;
+	}
+
+	key = 0;
+	//While para as chaves
+	//For para percorrer o vetor com essa chave
+	//Volta pro while e soma a chave
+
+	while (key <= keyMAx)
+	{
+		for (int i = 0; i < tamanho; i++)
+		{
+			modKey = key % tamanho;
+			vet2[i] = modKey;
+			if (vet[i] == vet2[i])
+			{
+				vet3[i] += 1;
+			}
+			
+			key += 1;
+		 }
+
+	}
+	for (int i = 0; i < tamanho; i++)
+	{
+		//std::cout << "Colisoes: " << vet3[i] << std::endl;
+		fprintf(saida, "Colisoes por chave <%d>: \t%d\n", i ,vet3[i]);
+	}
+
+	fclose(saida);
+
+	return 0;
+}
